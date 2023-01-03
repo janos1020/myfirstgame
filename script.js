d46f9a3f59db3e83fdcf2412f18f1d7ce17adc51
janos1020 <121908033+janos1020@users.noreply.github.com>
2023-01-04 00:22:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉Correct Number';
+document.querySelector('.number').textContent = '13';
+document.querySelector('.score').textContent = '10';
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+
+const secretNumber = Math.trunc(Math.random()*20+1);
+
+document.querySelector('.number').textContent = secretNumber
+
+
+
+document.querySelector('.check').addEventListener('click', function () {
+    
+   // console.log(document.querySelector('.guess').value);
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(typeof guess);
+    if (!guess) {
+        document.querySelector('.message').textContent = 'no number 😢'
+
+    }
+    else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉Correct Number';
+    }
+    else if (guess !== secretNumber) {
+        document.querySelector('.message').textContent = 'Wrong number 😢';
+    }
+})
